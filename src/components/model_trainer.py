f3ed70391117706faa4b0954f6690838d078f10e
Illia <2004lion2004@gmail.com>
2023-08-22 19:12:59 +0200

    minor changes

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -202,7 +202,6 @@ class ModelTrainer:
                 reg_alpha=trial.suggest_float("reg_alpha", 0, 1),
                 reg_lambda=trial.suggest_float("reg_lambda", 0, 1),
                 tree_method="gpu_hist",
-                objective="reg:absoluteerror",
             )
         elif model_name == "rf":
             model = RandomForestRegressor(
