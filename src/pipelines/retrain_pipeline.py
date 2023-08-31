08909d65bce3db6627154a19f77a569869eca430
Illia <2004lion2004@gmail.com>
2023-08-31 20:27:00 +0200

    retrain_pipeline fix

--- src/pipelines/retrain_pipeline.py
+++ src/pipelines/retrain_pipeline.py
@@ -90,7 +90,7 @@ def model_training(result):
     train_test_data_obj = result["train_test_data"]
     model_params = ModelParams()
     ModelTrainer(train_test_data_obj).train_and_log_model(
-        model_params.model_name, model_params.params
+        model_params.model_name, custom_params=model_params.params
     )
 
 
