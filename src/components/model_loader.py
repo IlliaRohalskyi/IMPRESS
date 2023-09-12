74d372fb69b0de023e30ee66d371e3541099d0db
Illia <2004lion2004@gmail.com>
2023-09-12 21:53:26 +0200

    WIP fixing xgboost loading

--- src/components/model_loader.py
+++ src/components/model_loader.py
@@ -9,17 +9,33 @@ as a dictionary, and retrieve these models for predictions.
 import os
 import shutil
 import sys
-from typing import Tuple
+from dataclasses import dataclass
+from typing import Tuple, Union
 
 import mlflow
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import get_project_root, load_pickle
 
 
-def save_models(
-    model_names: Tuple[str] = ("xgb", "rf"),
+@dataclass
+class ModelAndScalers:
+    """
+    This dataclass stores models and scalers associated with a specific model
+    """
+
+    model: Union[RandomForestRegressor, XGBRegressor]
+    model_name: str
+    feature_scaler: MinMaxScaler
+    target_scaler: MinMaxScaler
+
+
+def get_models(
+    model_names: Tuple[str] = ("rf", "xgb"),
     models_path: str = os.path.join(get_project_root(), "ml_downloads"),
     save: bool = False,
     load: bool = True,
@@ -59,12 +75,38 @@ def save_models(
                         dst_path=os.path.join(models_path, f"{model_name}"),
                     )
         if load:
-            models_and_scalers = {}
+            models_and_scalers = []
             for model_name in model_names:
+                model_and_scalers = ModelAndScalers
+
+                model_and_scalers.model_name = model_name
+
                 model_path = os.path.join(
-                    models_path, model_name, "models", "model.pkl"
+                    models_path, model_name, "models", model_name, "model.pkl"
                 )
-                model = load_pickle(model_path)
+                model = (
+                    XGBRegressor().load_model(model_path)
+                    if model_name == "xgb"
+                    else load_pickle(model_path)
+                )
+                print(model)
+                model_and_scalers.model = model
+
+                feature_scaler_path = os.path.join(
+                    models_path, model_name, "scalers", "feature_scaler.pkl"
+                )
+                target_scaler_path = os.path.join(
+                    models_path, model_name, "scalers", "target_scaler.pkl"
+                )
+
+                feature_scaler = load_pickle(feature_scaler_path)
+                model_and_scalers.feature_scaler = feature_scaler
+
+                target_scaler = load_pickle(target_scaler_path)
+                model_and_scalers.target_scaler = target_scaler
+
+                models_and_scalers.append(model_and_scalers)
+            return models_and_scalers
 
     except Exception as error_message:
         logging.error(f"Model loader failed: {error_message}")
@@ -72,4 +114,4 @@ def save_models(
 
 
 if __name__ == "__main__":
-    save_models(return_models=False)
+    a, b = get_models(load=True)
